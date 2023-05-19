47b97ef005db144343a36658af136b1ea01bf407
IH Syed <ihsyedscc@gmail.com>
2023-05-19 20:20:16 +0500

    Second Commit with Nono JPG

--- Desktop/MetaVerse/REact/reactcomponentsapp1/app/components/Profile.tsx
+++ Desktop/MetaVerse/REact/reactcomponentsapp1/app/components/Profile.tsx
@@ -1,3 +1,10 @@
+import Image from "next/image"
+
 export default function Profile() {
-    return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherinen Johnson" ></img>
+    //return <img src="https://i.imgur.com/MK3eW3Am.jpg" alt="Katherinen Johnson" ></img>
+
+
+    return <img src = "./Nono.jpg" alt = "Nono" width = {100} height={100} />
+
+
   }
